24333cc92883774deae09e0a7fb86c322a301adc
yamdev07 <yamdyamd080@gmail.com>
2025-10-28 18:42:13 +0100

    users managment

---
+++ mobilemoney-app/middleware/auth.js
@@ -0,0 +1,16 @@
+const jwt = require('jsonwebtoken');
+
+// Middleware pour vérifier le token
+module.exports = function (req, res, next) {
+  const token = req.header('Authorization')?.replace('Bearer ', '');
+
+  if (!token) return res.status(401).json({ message: 'Pas de token, accès refusé' });
+
+  try {
+    const decoded = jwt.verify(token, 'SECRET_KEY'); // remplace 'SECRET_KEY' par ton secret
+    req.user = decoded; // req.user contiendra id, role, email, etc.
+    next();
+  } catch (err) {
+    res.status(401).json({ message: 'Token invalide' });
+  }
+};
